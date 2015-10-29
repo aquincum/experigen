a41fce7b3905b7d7f9da6aa6ea5a78bbcce47dcb
aquincum <daniel@szeredi.hu>
2015-10-28 21:27:38 -0400

    Oops, accidental global

--- web/plugins/expanded_usercode.js
+++ web/plugins/expanded_usercode.js
@@ -2,7 +2,7 @@
 
 if(Experigen.settings.online){
 	var jsonp_url = Experigen.settings.databaseServer + "getuserid.cgi?experimentName=" + Experigen.settings.experimentName  + "&sourceurl=" + encodeURIComponent(window.location.protocol + "//" + window.location.hostname + window.location.pathname); // we get the user ID based from the server
-	innerLoad = function(data){
+	var innerLoad = function(data){
 		if(Experigen.userFileName === ""){
 			setTimeout(function(){ innerLoad(data); }, 500);
 			return;
