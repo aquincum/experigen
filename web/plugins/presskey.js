9b43ea9ac97b3bc5aac216499b7f9a883b4d31fa
aquincum <daniel@szeredi.hu>
2015-11-01 17:41:22 -0500

    Presskey plugin problems seemingly solved
    Needed to stop propagation, and test whether tarrget button is visible.
    Also, $(document).off("keyup") made no sense.

--- web/plugins/presskey.js
+++ web/plugins/presskey.js
@@ -1,14 +1,18 @@
-/** BUGGY YET */
+/** BUGGY YET 
+ * update 11/1/15: not as buggy anymore
+ */
+
 
 Experigen.registerPlugin({
-	extendtrial: function(trial) {
-		trial.attachKey = function(keysbuttons){
-			$(document).on("keyup", function(event){
-				if(keysbuttons[event.which]){
-					$(keysbuttons[event.which]).click();
-					$(document).off("keyup");
-				}
-			});
-		};
-	}
+    extendtrial: function(trial) {
+	trial.attachKey = function(keysbuttons){
+	    $(document).on("keyup", function(event){
+		if(keysbuttons[event.which] && $(keysbuttons[event.which]).is(":visible")){
+		    $(keysbuttons[event.which]).click();
+		}
+		event.preventDefault();
+		event.stopPropagation();
+	    });
+	};
+    }
 });
