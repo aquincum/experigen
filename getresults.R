6d57a558cf5b4cb8b36cc7e82fc35ea2d39a8b81
aquincum <daniel@szeredi.hu>
2016-03-02 16:32:35 -0500

    now with Rexperigen!

--- getresults.R
+++ getresults.R
@@ -1,34 +1,49 @@
-# where your experiment is hosted
-# removing initial "http://" and tildes
-# and substituting slashes, etc. with periods
-# no hyphens/dashes, alas (known bug)
-experigen.sourceURL = "www.awesomeuniversity.edu.iamawesome.questionnaire"
-# the following information comes from your settings.js file:
-experigen.experimentName = "Default"
-experigen.database = "http://db.phonologist.org/"
+if(!require(Rexperigen)){
+    install.packages("Rexperigen")
+    library(Rexperigen)
+}
 
-# first, send some info to the server with the current 
-# sourceURL and experimentName by submitting at least one screen
-# to the server.
-# otherwise, the server will return an error message
+## The Experigen server you're using 
+experigen.database <- "http://db.phonologist.org/"
 
-# check for usage of the experiment (number of page views per participant)
-experigen.users  =  paste(experigen.database, "users.cgi?experimentName=", experigen.experimentName, "&sourceurl=", experigen.sourceURL, sep="")
-read.csv(experigen.users, sep="\t")
+setExperigenServer(experigen.database)
+
+## where your experiment is hosted
+## removing initial "http://" and tildes
+## and substituting slashes, etc. with periods
+## no hyphens/dashes, alas (known bug)
+##
+## With the newer server version (>=2), the server
+## can help cleaning the URL
+experigen.sourceURL <-
+    ifelse(versionMain() < 2,
+           "www.awesomeuniversity.edu.iamawesome.questionnaire",
+           cleanURL("http://www.awesomeuniversity.edu/~iamawesome/questionnaire")
+           )
+
+## the following information comes from your settings.js file:
+experigen.experimentName <- "Default"
+
+## first, send some info to the server with the current 
+## sourceURL and experimentName by submitting at least one screen
+## to the server.
+## otherwise, the server will return an error message
+
+## check for usage of the experiment (number of page views per participant)
+experigen.users <- getUsers(experigen.sourceURL, experigen.experimentName)
 
 
-# read the experimental results from the server
-experigen.url  =  paste(experigen.database, "makecsv.cgi?experimentName=", experigen.experimentName, "&sourceurl=", experigen.sourceURL, sep="")
-xp  = read.csv(experigen.url, sep="\t")
+## read the experimental results from the server
+xp <- downloadExperiment(experigen.sourceURL, experigen.experimentName)
 xp$time = as.POSIXct(strptime(as.character(xp$time), "%a %b %d %H:%M:%S %Y"))
-meta = read.csv(paste(experigen.url, "&file=demographics.csv", sep=""), sep="\t")
+meta  <- downloadExperiment(experigen.sourceURL, experigen.experimentName, "demographics.csv")
 meta$time = as.POSIXct(strptime(as.character(meta$time), "%a %b %d %H:%M:%S %Y"))
 
-# assuming all went well, write to disk
-# so that the results are saved even after the database server is gone
-# it would be unwise not to keep a local copy of your results
+## assuming all went well, write to disk
+## so that the results are saved even after the database server is gone
+## it would be unwise not to keep a local copy of your results
 write.csv(xp, "xp.csv")
 write.csv(meta, "meta.csv")
 
-# optional cleanup: remove all variables that begin with "experigen."
+## optional cleanup: remove all variables that begin with "experigen."
 rm(list=ls(pattern="^experigen."))
